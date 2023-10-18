e79871c256fb7bf487a29a44d3a7a1c3ff839877
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-10-18 16:01:06 -0300

    video 3.2 - mudar o context pro dao e migrar os métodos obs: atualização dos métodos deletar e atualizar validação de existente

--- ScreenSound/Banco/ArtistaDAL.cs
+++ ScreenSound/Banco/ArtistaDAL.cs
@@ -12,55 +12,10 @@ namespace ScreenSound.Banco
 {
     internal class ArtistaDAL: EntityDAL<Artista> 
     {
-        private readonly ScreenSoundContext context;
 
-        public ArtistaDAL(ScreenSoundContext context)
-        {
-            this.context = context;
-        }
+        public ArtistaDAL(ScreenSoundContext context) : base(context) { }
 
-        public override IEnumerable<Artista> Listar()
-        {
-            return context.Artistas.ToList();
-        }
-
-        public override void Adicionar(Artista objeto)
-        {
-            context.Set<Artista>().Add(objeto);
-            context.SaveChanges();
-        }
-
-        public void Deletar(int id)
-        {
-            var artista = context.Artistas.Find(id);
-            if(artista != null)
-            {
-                context.Artistas.Remove(artista);
-                context.SaveChanges();
-                Console.WriteLine("Artista removido com sucesso.");
-            } else
-            {
-                Console.WriteLine("Artista não encontrado, tente outro Id.");
-            }
-        }
-
-        public void Atualizar(int id,  string nome, string bio)
-        {
-            if (context.Artistas.Any(a => a.Id == id))
-            {
-                var artistaEditado = new Artista(nome, bio, id);
-                context.Artistas.Update(artistaEditado);
-                context.SaveChanges();
-                Console.WriteLine("Artista atualizado com sucesso.");
-            }
-            else
-            {
-                Console.WriteLine("Artista não encontrado, tente outro Id.");
-            }
-
-        }
-
-        public Artista RecuperarPeloNome(string nome)
+        public Artista? RecuperarPeloNome(string nome)
         {
             var artista = context.Artistas.FirstOrDefault(a => a.Nome == nome);
             if (artista != null)
@@ -75,6 +30,6 @@ namespace ScreenSound.Banco
 
         }
 
-        
+
     }
 }
--- ScreenSound/Banco/EntityDAL.cs
+++ ScreenSound/Banco/EntityDAL.cs
@@ -1,4 +1,6 @@
-﻿using ScreenSound.Context;
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using ScreenSound.Context;
+using ScreenSound.Modelos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +9,41 @@ using System.Threading.Tasks;
 
 namespace ScreenSound.Banco
 {
-    internal abstract class EntityDAL<T> 
+    internal abstract class EntityDAL<T> where T : class
     {
+        protected readonly ScreenSoundContext context;
 
-        public abstract IEnumerable<T> Listar();
-        public abstract void Adicionar(T objeto);
+        protected EntityDAL(ScreenSoundContext context)
+        {
+            this.context = context;
+        }
+
+        public virtual IEnumerable<T> Listar()
+        {
+            return context.Set<T>().ToList();
+        }
+        public virtual void Adicionar(T objeto)
+        {
+            context.Set<T>().Add(objeto);
+            context.SaveChanges();
+        }
+
+        public virtual void Deletar(T objeto)
+        {
+            context.Set<T>().Remove(objeto);
+            context.SaveChanges();
+            Console.WriteLine($"{typeof(T)} removido com sucesso.");
+            
+        }
+
+        public virtual void Atualizar(T objeto)
+        {
+
+            context.Set<T>().Update(objeto);
+            context.SaveChanges();
+            Console.WriteLine($"{typeof(T)} atualizado com sucesso.");
+
+        }
 
     }
 }

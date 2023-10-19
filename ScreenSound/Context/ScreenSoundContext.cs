952ce615e393030cf9c20ce8b77e7c4ffbf9a7c9
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2023-10-19 17:18:24 -0300

    video 4.1

--- ScreenSound/Context/ScreenSoundContext.cs
+++ ScreenSound/Context/ScreenSoundContext.cs
@@ -14,7 +14,7 @@ namespace ScreenSound.Context
         public DbSet<Musica> Musicas { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound3;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
             optionsBuilder.UseSqlServer(connectionString);
         }
     }
--- ScreenSound/Modelos/Musica.cs
+++ ScreenSound/Modelos/Musica.cs
@@ -10,6 +10,7 @@ public class Musica
 
     public string Nome { get; set; }
     public int Id { get; set; } = 0;
+    
 
     public void ExibirFichaTecnica()
     {
